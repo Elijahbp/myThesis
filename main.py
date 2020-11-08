ca50034b9e8085016962b70283c0bec218ce31a8
elijahbp <elijah.b.i.p@gmail.com>
2020-11-08 23:57:19 +0400

    OK

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import speech_recognition as sr
 def test_microfone():
     mic = sr.Microphone(device_index=1)
     recog = sr.Recognizer()
-    while True:
-        with mic as audio_file:
-            print('Speech to microphone')
-            audio = recog.listen(source=audio_file,phrase_time_limit=10)
+    with mic as audio_file:
+        print('Speech to microphone')
+        audio = recog.listen(source=audio_file,phrase_time_limit=10)
 
-            print("Converting Speech to Text...")
-            text_from_speech = recog.recognize_sphinx(audio_data=audio, language='ru-RU')
-            print("You said: " + text_from_speech)
-            if text_from_speech == "выйти":
-                print('Exit from speech-to-text program')
-                break
+        print("Converting Speech to Text...")
+        text_from_speech = recog.recognize_sphinx(audio_data=audio, language='ru-RU')
+        print("You said: " + text_from_speech)
+        if text_from_speech == "выйти":
+            print('Exit from speech-to-text program')
 
 if __name__ == '__main__':
     test_microfone()
